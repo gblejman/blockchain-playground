fb7217b7abb2a974d46c8047308079b365578720
German Blejman <gblejman@gmail.com>
2023-07-28 22:39:43 -0300

    add dotenv

--- scripts/b-simple-storage/deploy-vanilla-ethers.js
+++ scripts/b-simple-storage/deploy-vanilla-ethers.js
@@ -1,5 +1,6 @@
 // Requiring ethers directly intead of hardhat just for learning purposes - injected ethers by hardhat plugin has same api
 const ethers = require('ethers');
+require('dotenv').config();
 
 /**
  * Manual sample of connecting to hardhat network and deploying a contract
@@ -7,8 +8,8 @@ const ethers = require('ethers');
 const main = async () => {
   console.log('Run npx hardhat node to start the hardhat network...');
 
-  const accountKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'; // hardhat account[0]
-  const provider = new ethers.JsonRpcProvider('http://127.0.0.1:8545/'); // printed when running `npx hardhat node`
+  const accountKey = process.env.PRIVATE_KEY; // hardhat account[0]
+  const provider = new ethers.JsonRpcProvider(process.env.LOCALHOST_RPC_URL); // printed when running `npx hardhat node`
   const wallet = new ethers.Wallet(accountKey, provider);
   const { abi, bytecode } = require('../../artifacts/contracts/b-simple-storage/SimpleStorage.sol/SimpleStorage.json'); // compiled artifact after `npx hardhat compile`
   const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
@@ -18,6 +19,10 @@ const main = async () => {
   console.log('Deployed contract:', contract);
   console.log('Deployment tx:', contract.deploymentTransaction());
   console.log('Contract address:', contract.target);
+
+  console.log('Contract value:', await contract.get());
+  await contract.set(5);
+  console.log('Contract value:', await contract.get());
 };
 
 main()
